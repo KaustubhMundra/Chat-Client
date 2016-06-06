6d88348b59d22e4330a9955b23bd066515f7286d
KaustubhMundra <kaustubh.mundra3@gmail.com>
2016-06-07 04:09:15 +0530

    updated index.js

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
+var path = require('path');
+ 
+// Initialize appication with route / (that means root of the application)
 app.get('/', function(req, res){
+  var express=require('express');
+  app.use(express.static(__dirname));
   res.sendFile(__dirname + '/index.html');
 });
-
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+// app.get('/', function(req, res){
+//   var express=require('express');
+//   app.use(express.static(path.join(__dirname)));
+//   res.sendFile(path.join(__dirname, '../chat-application', 'index.html'));
+// });
+ 
+// Register events on socket connection
+io.on('connection', function(socket){ 
+  socket.on('chatMessage', function(from, msg){
+    io.emit('chatMessage', from, msg);
+  });
+  socket.on('notifyUser', function(user){
+    io.emit('notifyUser', user);
   });
 });
 
+ 
+// Listen application request on port 3000
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
-
